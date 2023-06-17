5b745ac4b45c76e103b11e790201ff93fe88fded
nastyam1994 <nastyam_94@bk.ru>
2023-06-17 23:58:37 +0800

    Seed the db. Create Index method in Events controller. Add Index view for Events

--- Gemfile
+++ Gemfile
@@ -58,6 +58,8 @@ group :development, :test do
 
 end
 
+gem "faker"
+
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::EventsController < Api::V1::BaseController
+  # before_action :set_event, only: [ :show ]
+
+  def index
+    @events = Event.all
+  end
+
+  private
+  def set_event
+    @event = Event.find(params[:id])
+  end
 end
