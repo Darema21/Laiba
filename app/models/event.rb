94a39a26c2a91b99cfefd12fddddce04ddb26dc2
nastyam1994 <nastyam_94@bk.ru>
2023-06-22 19:06:59 +0800

    Update Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  has_many :users, through: :bookings
 
   validates :title, presence: true
   validates :description, presence: true
