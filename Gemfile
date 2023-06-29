d8080118375339e44b0555f402a46314816dff81
Darema Dzhold <darema.dzhold@outlook.com>
2023-06-29 22:33:31 +0800

    created .env file with keys, adjusted model and controller to allow image upload

--- Gemfile
+++ Gemfile
@@ -59,6 +59,7 @@ group :development, :test do
 end
 
 gem "faker"
+gem 'cloudinary'
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -49,7 +49,7 @@ class Api::V1::EventsController < Api::V1::BaseController
   end
 
   def event_params
-    params.require(:event).permit(:title, :description, :address, :start_time, :end_time, :category, :user_id)
+    params.require(:event).permit(:title, :description, :address, :start_time, :end_time, :category, :user_id, :image)
   end
 
   def render_error
@@ -58,3 +58,5 @@ class Api::V1::EventsController < Api::V1::BaseController
   end
 
 end
+
+# darema 6.29 line 52 added :image
